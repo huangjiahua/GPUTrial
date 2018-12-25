29383bcea0c2ad10c8a3d2185622324fe4d9a903
lwhay <lwh@whu.edu.cn>
2018-12-25 22:27:58 +0800

    Naive allocator.

--- GPUTrial/MemAollocation/Allocation.cuh
+++ GPUTrial/MemAollocation/Allocation.cuh
@@ -11,6 +11,8 @@
 
 #define SLOT_SIZE (1 << 10)
 
+#define SLOT_LIMIT (TOTAL_SIZE / SLOT_SIZE)
+
 #define PARALLEL_DEGREE (8)
 
 #define ITER_ROUND (TOTAL_SIZE / SLOT_SIZE / PARALLEL_DEGREE)
@@ -24,6 +26,10 @@
 #define PSEUDO_ROUND 100
 
 #define SMALLOC 0
+
+#define CPU_DYNAMIC 1
+
+#define SLOT_START(p, x)    (p + (bigint)x * SLOT_SIZE)
 
 typedef unsigned long long bigint;
 
@@ -70,12 +76,64 @@ struct cpu_memory_alloc {
         cout << "Deallocation: " << dealcTime << endl;
     }
 
+    char *mempool;
+
+    long *memindex;
+
+    void dynamic_worker() {
+        /*printf("CPU enter %lld\n", ITER_ROUND);*/
+        long index = 0;
+        long hit;
+        long *cached = new long[ITER_ROUND];
+        for (int r = 0; r < PSEUDO_ROUND; r++) {
+            long cidx = 0;
+            for (int i = 0; i < ITER_ROUND; i++) {
+                do {
+                    // stores (old == compare ? val : old) to &old, as well as returns old.
+                    hit = InterlockedCompareExchange(memindex + index, index, -1);
+                    index = (index + 1) % SLOT_LIMIT;
+                } while (hit == -1);
+                cached[cidx++] = hit;
+                bigint *slot = (bigint *) SLOT_START(mempool, hit);
+                /*printf("\t%d %d %llu %lld %d %d\n", hit, index, ((char *) slot - mempool) / SLOT_SIZE, SLOT_LIMIT,
+                (SLOT_SIZE / OPT_SIZE), INNER_ROUND);*/
+                for (int j = 0; j < INNER_ROUND; j++) {
+                    slot[j] = j;
+                }
+            }
+            /* printf("!!!!%d %d\n", r);*/
+            // InterlockedCompareExchange(&old, compare, val) stores (old == compare ? val : old) to &old, and returns old.
+            // todo We can release a page directed by an index for the positions of memindex.
+            cidx = 0;
+            for (int i = 0; i < ITER_ROUND; i++) {
+                do {
+                    hit = InterlockedCompareExchange(memindex + cached[cidx], -1, cached[cidx]);
+                } while (hit != -1);
+                cidx++;
+            }
+            /*printf("....%d %d %d %d %d\n", memindex[0], cached[0], memindex[SLOT_LIMIT - 1], cached[SLOT_LIMIT - 1],
+                   index);*/
+        }
+        delete[] cached;
+    }
+
     void cpu_allocation() {
         thread *threads = new thread[PARALLEL_DEGREE];
         clock_t begin;
         begin = clock();
+#if CPU_DYNAMIC
+        mempool = new char[TOTAL_SIZE];
+        memindex = new long[SLOT_LIMIT];
+        for (int i = 0; i < SLOT_LIMIT; i++) {
+            memindex[i] = i;
+        }
+#endif
         for (int i = 0; i < PARALLEL_DEGREE; i++) {
+#if CPU_DYNAMIC
+            threads[i] = thread(&cpu_memory_alloc::dynamic_worker, this);
+#else
             threads[i] = thread(&cpu_memory_alloc::cpu_worker, this);
+#endif;
         }
         for (int i = 0; i < PARALLEL_DEGREE; i++) {
             threads[i].join();
@@ -83,38 +141,48 @@ struct cpu_memory_alloc {
         clock_t finish = clock();
         cout << "CPU: " << (((double) finish - begin) / CLOCKS_PER_SEC) << endl;
         delete[] threads;
+#if CPU_DYNAMIC
+        delete[] memindex;
+        delete[] mempool;
+#endif
     }
 };
 
-#define SLOT_LIMIT          (TOTAL_SIZE / SLOT_SIZE)
-#define WARP_SIZE           1/*16*/ //Boundary 10/11, denoting M1200 has 10 MXMs?
-#define THREAD_NUM          1
+#define WARP_SIZE           8/*16*/ //Boundary 10/11, denoting M1200 has 10 MXMs?
+#define THREAD_NUM          1024
 #define LOCAL_NUM           (TOTAL_SIZE / (WARP_SIZE * THREAD_NUM) / SLOT_SIZE)
 #define CMALLOC             1
-#define SLOT_START(p, x)    (p + (bigint)x * SLOT_SIZE)
+#define CUDA_STATIC_MEM     0
 
 __global__
+#if CUDA_STATIC_MEM
+void cudaDynamicAlloc(char *mempool, int *memindex, int *caches) {
+    int* cached = caches + (blockIdx.x * blockDim.x + threadIdx.x) * LOCAL_NUM;
+#else
 void cudaDynamicAlloc(char *mempool, int *memindex) {
-    printf("GPU enter %d\n", LOCAL_NUM);
+    int cached[LOCAL_NUM];
+#endif
+    //printf("GPU enter %d\n", LOCAL_NUM);
     int index = 0;
     int hit;
-    int cached[LOCAL_NUM];
-    int cidx = 0;
     for (int r = 0; r < PSEUDO_ROUND; r++) {
+        int cidx = 0;
         for (int i = 0; i < LOCAL_NUM; i++) {
             do {
                 hit = atomicCAS(memindex + index, index, -1);
                 index = (index + 1) % SLOT_LIMIT;
             } while (hit == -1);
             cached[cidx++] = hit;
-            /*bigint *slot = (bigint *) SLOT_START(mempool, hit);
-            printf("\t%d %d %llu %lld %d %d\n", hit, index, ((char *) slot - mempool) / SLOT_SIZE, SLOT_LIMIT,
-                   (SLOT_SIZE / OPT_SIZE), INNER_ROUND);
+#if OPERATING
+            bigint *slot = (bigint *) SLOT_START(mempool, hit);
+            /*printf("\t%d %d %llu %lld %d %d\n", hit, index, ((char *) slot - mempool) / SLOT_SIZE, SLOT_LIMIT,
+                   (SLOT_SIZE / OPT_SIZE), INNER_ROUND);*/
             for (int j = 0; j < INNER_ROUND; j++) {
                 slot[j] = j;
-            }*/
+            }
+#endif
         }
-        printf("!!!!%d %d\n", r);
+        printf("!!!!%d %d\n", r, index);
         // atomicCAS(&old, compare, val) stores (old == compare ? val : old) to &old, as well as returns old.
         // todo We can release a page directed by an index for the positions of memindex.
         cidx = 0;
@@ -124,6 +192,8 @@ void cudaDynamicAlloc(char *mempool, int *memindex) {
             } while (hit != -1);
             cidx++;
         }
+        printf("....%d %d %d\n", r, index, (blockIdx.x * blockDim.x + threadIdx.x));
+        //__syncthreads();
     }
 }
 
@@ -134,18 +204,28 @@ void gpu_dynamic() {
     for (int i = 0; i < SLOT_LIMIT; i++) {
         index[i] = i;
     }
+#if CUDA_STATIC_MEM
+    int *caches;
+#endif
     cudaEvent_t beg, en;
     cudaEventCreate(&beg);
     cudaEventCreate(&en);
     cudaMalloc((void **) &mempool, TOTAL_SIZE);
     cudaMalloc((void **) &memindex, sizeof(int) * SLOT_LIMIT);
+#if CUDA_STATIC_MEM
+    cudaMalloc((void **) &caches, WARP_SIZE * THREAD_NUM * LOCAL_NUM * sizeof(int));
+#endif
     cout << "Total bytes: " << (TOTAL_SIZE + sizeof(int) * SLOT_LIMIT) << " " << SLOT_LIMIT << " " << LOCAL_NUM << endl;
     cudaMemcpy(memindex, index, sizeof(int) * SLOT_LIMIT, cudaMemcpyHostToDevice);
     delete[] index;
     clock_t begin;
     begin = clock();
     cudaEventRecord(beg);
+#if CUDA_STATIC_MEM
+    cudaDynamicAlloc << < WARP_SIZE, THREAD_NUM >> > (mempool, memindex, caches);
+#else
     cudaDynamicAlloc << < WARP_SIZE, THREAD_NUM >> > (mempool, memindex);
+#endif
     cudaEventRecord(en);
     cudaEventSynchronize(en);
 
@@ -157,6 +237,9 @@ void gpu_dynamic() {
     cout << "CPU-GPU: " << (((double) finish - begin) / CLOCKS_PER_SEC) << endl;
     cudaFree(mempool);
     cudaFree(memindex);
+#if CUDA_STATIC_MEM
+    cudaFree(caches);
+#endif
 }
 
 __global__
--- GPUTrial/MemAollocation/AllocationTest.cu
+++ GPUTrial/MemAollocation/AllocationTest.cu
@@ -5,17 +5,9 @@
 #include "Allocation.cuh"
 
 int main(int argc, char **argv) {
-    for (int i = 0; i < ITER_ROUND; i++) {
-        static bigint POOL[ITER_ROUND][INNER_ROUND];
-        for (int j = 0; j < ITER_ROUND; j++) {
-            for (int k = 0; k < INNER_ROUND; k++) {
-                POOL[j][k] = k;
-            }
-        }
-    }
     cout << "Begin" << endl;
     struct cpu_memory_alloc allocator;
-    //allocator.cpu_allocation();
+    allocator.cpu_allocation();
     gpu_dynamic();
     //gpu_alloc();
     return 0;
